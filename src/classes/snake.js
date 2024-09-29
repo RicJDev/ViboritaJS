3df64c75cd17a02a9a6eb8d64253938d7dc13c42
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-09-29 10:40:48 -0400

    Reordenando ficheros y modificando la clase Snake

--- src/classes/snake.js
+++ src/classes/snake.js
@@ -1,7 +1,29 @@
+/**@typedef {'up' | 'down' | 'left' | 'right'} direction */
+
 class Snake {
   body = [{ x: 1, y: 1 }]
+
+  /**@type {direction} */
   direction = 'right'
-  tail = 2
+  size = 2
+  // speed = 1
+
+  movements = {
+    up: () => this.body[0].y--,
+    down: () => this.body[0].y++,
+    left: () => this.body[0].x--,
+    right: () => this.body[0].x++,
+  }
+
+  move() {
+    this.body.length = this.size + 2
+    const voidIndex = this.body.length - 1
+
+    for (let index = voidIndex; index > 0; index--) this.body[index] = { ...this.body[index - 1] }
+
+    const moveHead = this.movements[this.direction]
+    moveHead()
+  }
 }
 
 export { Snake }
