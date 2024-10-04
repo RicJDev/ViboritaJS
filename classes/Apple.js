192e0e2a461126cd8526cc6e794525f9a60330aa
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-10-04 18:53:24 -0400

    Subiendo clase Apple

---
+++ classes/Apple.js
@@ -0,0 +1,27 @@
+function defaultCallback(x, y) {
+  x = Math.floor(Math.random() * 30)
+  y = Math.floor(Math.random() * 30)
+
+  return { x, y }
+}
+
+class Item {
+  position = { x: 10, y: 20 }
+  isGeneretable = true
+
+  color = '#970'
+  gridValue = 6
+
+  generate(callback = defaultCallback) {
+    if (this.isGeneretable) {
+      this.isGeneretable = false
+
+      const { x, y } = callback(this.position.x, this.position.y) || this.position
+
+      this.position.x = x
+      this.position.y = y
+    }
+  }
+}
+
+export { Item }
