afc09076579d77ae8aa04050dcf5a347aec68da2
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-10-06 22:45:55 -0400

    Actualizando clases y corrigiendo error en la clase Snake

--- classes/Snake.js
+++ classes/Snake.js
@@ -2,31 +2,25 @@ class Snake {
   body = [{ x: 1, y: 1 }]
   size = 2
 
-  color = ''
+  color = '#2a8'
   gridValue = 1
 
+  direction = 'right'
   movements = {
-    up: () => snake.body[0].y--,
-    down: () => snake.body[0].y++,
-    left: () => snake.body[0].x--,
-    right: () => snake.body[0].x++,
-    none: () => {
-      snake.body[0] = snake.body[0]
-    },
+    up: () => this.body[0].y--,
+    down: () => this.body[0].y++,
+    left: () => this.body[0].x--,
+    right: () => this.body[0].x++,
+    none: () => {},
   }
 
-  direction = 'right'
-
   move() {
-    const moveHead = this.movements[this.direction]
     this.body.length = this.size + 2
-
     const voidIndex = this.body.length - 1
 
-    for (let i = voidIndex; i > 0; i--) {
-      this.body[i] = { ...this.body[i - 1] }
-    }
+    for (let i = voidIndex; i > 0; i--) this.body[i] = { ...this.body[i - 1] }
 
+    const moveHead = this.movements[this.direction] || this.movements.right
     moveHead()
   }
 }
