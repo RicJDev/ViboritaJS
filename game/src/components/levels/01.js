b24e9666a736d9587fd4578037675e651a437436
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-10-08 02:43:19 -0400

    Actualizando tablero básico

---
+++ game/src/components/levels/01.js
@@ -0,0 +1,1 @@
+import { basicBoard } from './basicBoard.js'
---
+++ game/src/components/levels/basicBoard.js
@@ -0,0 +1,5 @@
+const basicBoard = new Array(30)
+
+for (let i = 0; i < basicBoard.length; i++) basicBoard[i] = new Array(30).fill(0)
+
+export { basicBoard }
