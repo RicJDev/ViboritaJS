1a8c90d459e4fec280180b3ae5c47d412fc7f3bf
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2025-08-29 10:04:19 -0400

    usando pantalla en el main

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
-document.querySelector('#app').innerHTML = `
-  <canvas class="game"></canvas>
-`
+import { Screen } from './game/screen.js'
+
+document.querySelector('#app').innerHTML = `<canvas class="game"></canvas>`
+const screen = new Screen(document.querySelector('.game'))
+
+screen.background = '#329981'
