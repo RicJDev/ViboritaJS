1cb0ae9056f211e34b3c7e9b95bc4f90ded26671
Ricardo Marín <147381654+RicJDev@users.noreply.github.com>
2026-02-14 07:46:26 -0400

    chore: limpiando para empezar de cero

--- src/game/Level.js
+++ src/game/Level.js
@@ -1,9 +1,1 @@
-export class Level {
-  constructor({ apple, snake, obstacles }) {
-    this.apple = apple
-    this.snake = snake
-    this.obstacles = obstacles
-
-    this.elements = [this.obstacles, this.apple, this.snake]
-  }
-}
+export class Level {}
